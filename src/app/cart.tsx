47b6a63d817cb643a34f19c0d02c9ff69b05ba77
ezekiel <irechukwuezekiel@gmail.com>
2024-05-31 07:42:23 +0100

    shopping cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,16 +1,24 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { useCart } from "../providers/CartProvider";
 import CartListItem from "../components/CartListItem";
+import Button from "../components/Button";
 
 const CartScrren = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
 
   return (
-    <View>
+    <View style={{ padding: 10 }}>
       <FlatList
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
         contentContainerStyle={{ padding: 10, gap: 10 }}
+      />
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: "500" }}>
+        Total: ${total}
+      </Text>
+      <Button
+        // onPress={checkout}
+        text="Checkout"
       />
     </View>
   );
